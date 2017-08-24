9843ab2c59f1f639b3ba505843a1e06038921bc7
Chris Nethercott <ccnethercott@gmail.com>
2017-08-24 22:00:09 +0100

    this is a simple code that we will write out program off

--- src/logictemp.py
+++ src/logictemp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
 '''
 Psudo Code
 
-Everyminute from a cron job
+Everyminute from a cron job or a differnet time, idc. This script will run and hopefully put some values into a google spreadsheet.
+
+Run -> Get Temp IF(temp ± 1C) THEN input into spreadsheet else exit -> runs put into spreadsheet
 '''
